be74e03b3f5b77b97fa6957a94d9c15a5065c63e
srishanth rishi <srishanthrishi9@gmail.com>
2025-11-18 11:38:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Form submission alert
+document.getElementById("contact-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Thank you for reaching out, Rishi will get back to you soon!");
+});
